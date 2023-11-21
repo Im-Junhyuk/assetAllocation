508077c15e0e3b753871a60275bb75f492e869b7
Im-Jumhyuk <junhyuk1027@gmail.com>
2023-11-21 22:18:40 +0900

    tesr result dto updated

--- src/main/java/graduationproject/assetallocation/domain/dto/BacktestResultDTO.java
+++ src/main/java/graduationproject/assetallocation/domain/dto/BacktestResultDTO.java
@@ -1,21 +1,19 @@
 package graduationproject.assetallocation.domain.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Builder
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BacktestResultDTO {
-    Long returns;
 
-    Long volatility;
-
-    Long sharpe;
-
+    Double returns;
+    Double volatility;
+    Double sharpe;
     String graph;
 }
-//        "returns": 정수값,
-//        "volatility": 정수값,
-//        "sharpe": 정수값,
-//        "graph":"base64_encoded_string"
+//"returns": ,
+//        "volatility": ,
+//        "sharpe": ,
+//        "graph":"base6
